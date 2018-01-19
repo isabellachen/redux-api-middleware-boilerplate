263571ca8ce34ac407e9d28247953ae1efa0b333
Isabella Chen <miss.izzyc@gmail.com>
2018-01-19 12:40:45 +0100

    build($src): re-organised app component files
    moved app.js to component folder, created test and css folders

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import {Provider} from 'react-redux'
-// import store from './configureStore'
+import store from './configureStore'
+
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 
